1b6f749b19a06bce8f94ee7af3e3f0f51e2ee1dd
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-07 02:05:03 +0530

    express first app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const express = require("express"); // returns a function to create a backend/ web app
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello, Welcome to Express Demo app");
+});
+
+app.listen(8080, () => console.log("Express-demo is listening on port 8080"));
