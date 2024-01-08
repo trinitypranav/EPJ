33235571019258d3c6d4d3041a3978a545fe9da0
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-08 08:39:24 +0530

    capturing path params in a dynamic URL and returning data based on that

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ app.get("/api/courses", (req, res) => {
   res.status(200).send(courses);
 });
 
+app.get("/api/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((course) => course.id === parseInt(id));
+  if (!course) res.status(404).send(`Course with ${id} was not found`);
+
+  res.status(200).send(course);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`Express-demo is listening on port ${port}`)
