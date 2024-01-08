7c53d6c4666164bec30be29d8ea2864329b0c3e6
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-08 09:37:44 +0530

    POST request along with input validation using JOI

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express"); // returns a function to create a backend/ web app
+const Joi = require("joi");
+
 const app = express();
+app.use(express.json()); // middleware for handling JSON parsing of request body
 
 const courses = [
   { id: 1, name: "course1 - JavaScript" },
   { id: 2, name: "course2 - SQL" },
   { id: 3, name: "course3 - React" },
 ];
+
+const courseSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, Welcome to Express Demo app");
@@ -23,9 +30,21 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const id = req.params.id;
   const course = courses.find((course) => course.id === parseInt(id));
-  if (!course) res.status(404).send(`Course with ID ${id} was not found`);
+  if (!course)
+    return res.status(404).send(`Course with ID ${id} was not found`);
 
   res.status(200).send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const result = courseSchema.validate(req.body);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+
+  const newCourse = { ...req.body, id: courses.length + 1 };
+  courses.push(newCourse);
+  console.log(courses);
+  res.status(201).send(newCourse);
 });
 
 const port = process.env.PORT || 3000;
