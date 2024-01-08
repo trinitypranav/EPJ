91f318920fba9d72d1aa4eaa9a45d3557a62e00b
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-08 08:38:28 +0530

    handling GET requests using app.get()

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express"); // returns a function to create a backend/ web app
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1 - JavaScript" },
+  { id: 2, name: "course2 - SQL" },
+  { id: 3, name: "course3 - React" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello, Welcome to Express Demo app");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.status(200).send(courses);
 });
 
 const port = process.env.PORT || 3000;
