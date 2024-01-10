ef32f49263bf564a7b1a0f9733b84bb359edf300
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-11 00:22:32 +0530

    connecting courses route to mongodb with Joi and Mongoose data validations

--- routes/courses.js
+++ routes/courses.js
@@ -3,34 +3,31 @@ const router = express.Router();
 const Joi = require("joi");
 const Course = require("../models/courseModel");
 
-// this array contains all courses objects
-const courses = [
-  { id: 1, name: "course1 - JavaScript" },
-  { id: 2, name: "course2 - SQL" },
-  { id: 3, name: "course3 - React" },
-];
-
 // input validation schema for course object
 const courseSchema = Joi.object({
   name: Joi.string().min(2).required(),
+  author: Joi.string().min(2).max(55).required(),
+  tags: Joi.array(),
+  price: Joi.number().required(),
+  isPublished: Joi.boolean(),
 });
 
-router.get("/", (req, res) => {
-  const query = req.query;
-  if (query.name) {
-    const result = courses.filter((course) => course.name.includes(query.name));
-    return res.status(200).send(result);
+router.get("/", async (req, res) => {
+  try {
+    const result = await Course.find().sort("name");
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
   }
-  res.status(200).send(courses);
 });
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const course = courses.find((course) => course.id === parseInt(id));
-  if (!course)
-    return res.status(404).send(`Course with ID ${id} was not found`);
-
-  res.status(200).send(course);
+router.get("/:id", async (req, res) => {
+  try {
+    const document = await Course.findById(req.params.id);
+    res.status(200).send(document);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -39,40 +36,38 @@ router.post("/", async (req, res) => {
   if (result.error)
     return res.status(400).send(result.error.details[0].message);
 
-  // no error i.e. input is validated. Update the database and return the newly created course object
-  // const newCourse = { ...req.body, id: courses.length + 1 };
-  // courses.push(newCourse);
-  // console.log(courses);
-
   const newCourse = new Course(req.body);
-  console.log(newCourse);
   const response = await newCourse.save();
   console.log(response);
-
   res.status(201).send(newCourse);
 });
 
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
-  const courseIndex = courses.findIndex((course) => course.id === parseInt(id));
-  if (courseIndex === -1)
-    return res.status(404).send(`Course with ID ${id} was not found`);
+router.put("/:id", async (req, res) => {
+  try {
+    // Not required
+    // const { error } = courseSchema.validate(req.body);
+    // if (error) return res.status(400).send(`Bad Request`);
 
-  const { error } = courseSchema.validate(req.body);
-  if (error) return res.status(400).send(`Bad Request`);
-
-  courses[courseIndex] = { ...courses[courseIndex], ...req.body };
-  console.log(courses);
-  res.status(200).send(`Course with ID ${id} has been updated`);
+    // find the record with provided id and updated the specific part of the document as per provided request body
+    // no need to pass complete object here
+    const foundDocument = await Course.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { new: true }
+    );
+    res.status(200).send(foundDocument);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  const courseIndex = courses.findIndex((course) => course.id === parseInt(id));
-  if (courseIndex === -1)
-    return res.status(404).send(`Course with ID ${id} was not found`);
-
-  res.status(200).send(courses.splice(courseIndex, 1));
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedDocument = await Course.findByIdAndDelete(req.params.id);
+    res.status(200).send(deletedDocument);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
