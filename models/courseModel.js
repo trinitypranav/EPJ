646153d6612a7c151e5052d2ba071ca1f177d2a1
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-11 00:24:23 +0530

    Added more columns in course model with strict and advanced mongoose validations

--- models/courseModel.js
+++ models/courseModel.js
@@ -3,7 +3,26 @@ const mongoose = require("mongoose");
 const courseDocumentSchema = new mongoose.Schema({
   name: {
     type: String,
+    minLength: 2,
     require: true,
+  },
+  author: {
+    type: String,
+    minLength: 2,
+    maxLength: 55,
+    require: true,
+  },
+  tags: {
+    type: Array,
+  },
+  price: {
+    type: Number,
+    require: true,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+  },
+  isPublished: {
+    type: Boolean,
   },
 });
 
