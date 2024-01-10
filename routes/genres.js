bd8bdc9d19b808ccacef5692da2c7694fbcd841d
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-11 00:21:52 +0530

    connecting genre route to mongodb with Joi and Mongoose data validations

--- routes/genres.js
+++ routes/genres.js
@@ -1,77 +1,64 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const Genre = require("../models/genreModel");
 
-const genres = [
-  {
-    id: 1,
-    genre: "horror",
-    movies: ["US", "Get Out", "Psycho"],
-  },
-  {
-    id: 2,
-    genre: "action",
-    movies: ["Thor", "End Game"],
-  },
-];
-
-router.get("/", (req, res) => {
-  res.send(genres);
-});
-router.get("/:id", (req, res) => {
-  const name = genres.find((c) => c.id === parseInt(req.params.id));
-  res.send(name);
+const genreSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  movies: Joi.array(),
 });
 
-router.post("/", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(404).send(error.details[0].message);
+router.get("/", async (req, res) => {
+  try {
+    const result = await Genre.find();
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
   }
-  const addGenre = {
-    id: genres.length + 1,
-    genre: req.body.genre,
-    movies: req.body.movies,
-  };
-
-  genres.push(addGenre);
-  res.send(addGenre);
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const document = await Genre.findById(req.params.id);
+    res.status(200).send(document);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre) {
-    return res.status(404).send("Invalid ID");
+router.post("/", async (req, res) => {
+  try {
+    const { error } = genreSchema.validate(req.body);
+    if (error) {
+      return res.status(404).send(error.details[0].message);
+    }
+    const newDocument = new Genre(req.body);
+    const result = await newDocument.save();
+    res.status(201).send(result);
+  } catch (error) {
+    console.log(error);
   }
-
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(404).send(error.details[0].message);
-  }
-
-  genre.genre = req.body.genre;
-  genre.movies = req.body.movies;
-
-  res.send(genre);
 });
 
-router.delete("/:id", (req, res) => {
-  const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre) {
-    return res.status(404).send("Invalid ID");
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedDocument = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { new: true }
+    );
+    res.status(200).send(updatedDocument);
+  } catch (error) {
+    console.log(error);
   }
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
 });
 
-function validateGenre(genre) {
-  const genreSchema = Joi.object({
-    genre: Joi.string().required().min(3),
-    movies: Joi.array().items(Joi.string()).length(2).required(),
-  });
-
-  return genreSchema.validate(genre);
-}
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedDocument = await Genre.findByIdAndDelete(req.params.id);
+    res.status(200).send(deletedDocument);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
