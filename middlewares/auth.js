593b04bddca8713e5325868e059364d491e29d3e
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-15 14:57:43 +0530

    added auth middleware for validating 'x-auth'token' in the request headers

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access Denied. No Token Provided");
+
+  // we have a  token and we need to verify it
+  try {
+    const decoded = jwt.verify(token, process.env.jwtKey);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid Token");
+  }
+};
