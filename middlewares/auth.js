d8795109fcdc6085ae40007bbb897cd0cfd3e6db
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-15 15:18:48 +0530

    added log statements for viewing token and decoded values

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,12 @@ module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access Denied. No Token Provided");
 
+  console.log("token is ", token);
   // we have a  token and we need to verify it
   try {
     const decoded = jwt.verify(token, process.env.jwtKey);
     req.user = decoded;
+    console.log("decoded is ", decoded);
     next();
   } catch (error) {
     res.status(400).send("Invalid Token");
