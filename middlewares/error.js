4acbb8b891677a3320495bf511fba6624ca100cd
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-16 11:50:36 +0530

    added error middleware by Express and used winston for logging errors effectively

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,14 @@
+const winston = require("winston");
+
 module.exports = function (err, req, res, next) {
-    //log the exception
+  //log the exception using winston
+  winston.error(err.message, err);
+  //error
+  //warn
+  //info
+  //verbose
+  //debug
+  //silly
+
   res.status(500).send("something failed miserably ", err);
 };
