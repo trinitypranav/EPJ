6034cd82319c3f92acb807225d64935379fd4e0e
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-16 11:51:26 +0530

    removed try-catch blocks as express-async-errors will monkeypatch the handlers code at runtime

--- routes/genres.js
+++ routes/genres.js
@@ -10,58 +10,39 @@ const genreSchema = Joi.object({
   movies: Joi.array(),
 });
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await Genre.find().sort("name");
-    res.status(200).send(result);
-  } catch (error) {
-    next(error);
-  }
+router.get("/", async (req, res) => {
+  throw new Error("could not get genres");
+  const result = await Genre.find().sort("name");
+  res.status(200).send(result);
 });
 router.get("/:id", async (req, res) => {
-  try {
-    const document = await Genre.findById(req.params.id);
-    res.status(200).send(document);
-  } catch (error) {
-    console.log(error);
-  }
+  const document = await Genre.findById(req.params.id);
+  res.status(200).send(document);
 });
 
 router.post("/", auth, async (req, res) => {
-  try {
-    const { error } = genreSchema.validate(req.body);
-    if (error) {
-      return res.status(404).send(error.details[0].message);
-    }
-    const newDocument = new Genre(req.body);
-    const result = await newDocument.save();
-    res.status(201).send(result);
-  } catch (error) {
-    console.log(error);
+  const { error } = genreSchema.validate(req.body);
+  if (error) {
+    return res.status(404).send(error.details[0].message);
   }
+  const newDocument = new Genre(req.body);
+  const result = await newDocument.save();
+  res.status(201).send(result);
 });
 
 router.put("/:id", async (req, res) => {
-  try {
-    const updatedDocument = await Genre.findByIdAndUpdate(
-      req.params.id,
-      { ...req.body },
-      { new: true }
-    );
-    res.status(200).send(updatedDocument);
-  } catch (error) {
-    console.log(error);
-  }
+  const updatedDocument = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { ...req.body },
+    { new: true }
+  );
+  res.status(200).send(updatedDocument);
 });
 
 // only admin can delete genre. Passing 2 middlewares - first auth and then admin
 router.delete("/:id", [auth, admin], async (req, res) => {
-  try {
-    const deletedDocument = await Genre.findByIdAndDelete(req.params.id);
-    res.status(200).send(deletedDocument);
-  } catch (error) {
-    console.log(error);
-  }
+  const deletedDocument = await Genre.findByIdAndDelete(req.params.id);
+  res.status(200).send(deletedDocument);
 });
 
 module.exports = router;
