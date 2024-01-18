5aafd9be47b23a743884cb3bebb1b38e6e20726b
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-18 18:41:24 +0530

    Adding middleware and validations for Genres route

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require("../middlewares/validateObjectId");
 const auth = require("../middlewares/auth");
 const admin = require("../middlewares/admin");
 const express = require("express");
@@ -6,27 +7,30 @@ const Joi = require("joi");
 const Genre = require("../models/genreModel");
 
 const genreSchema = Joi.object({
-  name: Joi.string().required().min(3),
+  name: Joi.string().min(5).max(50).required(),
   movies: Joi.array(),
 });
 
 router.get("/", async (req, res) => {
-  throw new Error("could not get genres");
   const result = await Genre.find().sort("name");
   res.status(200).send(result);
 });
-router.get("/:id", async (req, res) => {
+
+router.get("/:id", validateObjectId, async (req, res) => {
   const document = await Genre.findById(req.params.id);
+  if (!document)
+    return res.status(404).send("The genre with the given Id does not exist");
   res.status(200).send(document);
 });
 
 router.post("/", auth, async (req, res) => {
   const { error } = genreSchema.validate(req.body);
   if (error) {
-    return res.status(404).send(error.details[0].message);
+    return res.status(400).send(error.details[0].message);
   }
-  const newDocument = new Genre(req.body);
-  const result = await newDocument.save();
+  const genre = new Genre(req.body);
+  const result = await genre.save();
+
   res.status(201).send(result);
 });
 
