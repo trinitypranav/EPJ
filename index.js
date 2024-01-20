494b8a0950adb0e96cb07626034a9b1534f301d0
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-20 23:35:56 +0530

    refactoring the code and creating startup folder

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-// external frameworks
 const express = require("express"); // returns a function to create a backend/ web app
-const helmet = require("helmet");
-const morgan = require("morgan");
-const dotenv = require("dotenv").config(); // loads env vars from .env into process.env object. console.log("what is this dotenv", dotenv);//{ parsed: { mongodb_url: 'mongodb://something' } }
-const error = require("./middlewares/error");
-
-// internal modules
-require("./configs/mongodbConfig");
-const logger = require("./middlewares/logger");
-const courses = require("./routes/courses");
-const home = require("./routes/home");
-const genres = require("./routes/genres");
-const users = require("./routes/users");
-
-// Express app setup
 const app = express();
-app.use(express.json()); // middleware for parsing request body JSON into JS object and adding it into req.body
-app.use(express.urlencoded({ extended: true })); // parsing urlencoded data in the request body. Sent by client in case of form submit
-app.use(express.static("public")); // serve static files from public folder
-app.use(logger); // custom middleware logger
-app.use(helmet()); // third-party middleware for securing Express apps by adding HTTP response headers
-app.use(morgan("tiny")); // HTTP request logger middleware for node.js i.e. GET /api/courses 200 106 - 1.043 ms
 
-// Routers
-app.use("/api/courses", courses);
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/users", users);
-
-// efficient error handling - error middleware
-app.use(error); // passing error handling middleware which sits at the end of request processing queue
-
-console.log("Running Environment: ", app.get("env")); // if not defined, it returns 'development' by default
-// console.log(process.env);
+// startup
+require("./startup/logging")(); // set up the logger
+require("./startup/routes")(app); // load all dependencies for the project
+require("./startup/config")(app); // check if we have all configs in place e.g. mongoUrl, jwtKey, etc
+require("./startup/mongodb")(); // connect to mongodb
+require("./startup/validations")(); // Joi objectId() validations for reference and embedding
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
+const server = app.listen(port, () =>
   console.log(`Express-demo is listening on port ${port}`)
 );
+
+module.exports = server;
