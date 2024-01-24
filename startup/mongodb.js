c32eb2de7211b8d9589fad719f22f215466f56ba
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-24 15:32:56 +0530

    logging mongourl

--- startup/mongodb.js
+++ startup/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function () {
-  //console.log(process.env);
+  console.log(process.env.mongodbUrl);
   mongoose.connect(process.env.mongodbUrl).then((result) => {
     winston.info(
       `Connected Successfully To MongoDB - ${process.env.mongodbUrl}`
