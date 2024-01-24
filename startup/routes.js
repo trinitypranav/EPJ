b712a4f9b4019d1a220ea46f957e1b4f77ef4fe6
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-24 17:32:17 +0530

    added a route for eatvilla API and enabled CORS

--- startup/routes.js
+++ startup/routes.js
@@ -3,9 +3,11 @@ const courses = require("../routes/courses");
 const home = require("../routes/home");
 const genres = require("../routes/genres");
 const users = require("../routes/users");
+const eatvilla = require("../routes/eatvilla");
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const error = require("../middlewares/error");
 
@@ -17,12 +19,13 @@ module.exports = function (app) {
   app.use(logger); // custom middleware logger
   app.use(helmet()); // third-party middleware for securing Express apps by adding HTTP response headers
   app.use(morgan("tiny")); // HTTP request logger middleware for node.js i.e. GET /api/courses 200 106 - 1.043 ms
-
+  app.use(cors());
   // Routers
   app.use("/api/courses", courses);
   app.use("/", home);
   app.use("/api/genres", genres);
   app.use("/api/users", users);
+  app.use("/api/eatvilla", eatvilla);
   // efficient error handling - error middleware
   app.use(error); // passing error handling middleware which sits at the end of request processing queue
 };
